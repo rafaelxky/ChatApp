e74b226ac834155432cb8f324d64080d0ebbaf47
Rafael Penacho <71713412+rafaelxky@users.noreply.github.com>
2025-03-12 14:48:32 +0000

    Delete SuperSecureChatApp/SuperSecureChatApp directory
    deleted dir

--- SuperSecureChatApp/SuperSecureChatApp/src/io/codeforall/kernelfc/Server/MyLittleBigMainClass.java
+++
@@ -1,9 +0,0 @@
-package io.codeforall.kernelfc.Server;
-
-public class MyLittleBigMainClass {
-
-    public static void main(String[] args) {
-        Server myLittleServer = new Server();
-        myLittleServer.start();
-    }
-}
--- SuperSecureChatApp/SuperSecureChatApp/src/io/codeforall/kernelfc/Server/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package io.codeforall.kernelfc.Server.commands;
-
-import io.codeforall.kernelfc.Server.ClientHandler;
-import io.codeforall.kernelfc.Server.Server;
-
-public abstract class Command {
-    public abstract void execute(ClientHandler clientHandler, Server server, String[] in);
-}
